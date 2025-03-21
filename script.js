4b84d9e21069e4d1b3882034c5cb6b7755a6d53b
Mateo <monorymateo1@gmail.com>
2025-03-21 10:34:35 -0500

    listo

---
+++ script.js
@@ -0,0 +1,17 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Ventas Tulua Cervalle
+TEL:+57 3118814976
+EMAIL;TYPE=WORK: ventastulua@cervalle.com 
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
